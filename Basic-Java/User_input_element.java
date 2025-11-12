75e85c910b04437417c3205a58693f2b63dfb3e1
SouravJana980 <sj493393@gmail.com>
2025-11-12 22:07:37 +0530

    Add User_input_element class for user input handling

---
+++ Basic-Java/User_input_element.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class User_input_element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //print statment
+        System.out.print("enter input element:");
+        //input statment
+        int element = sc.nextInt();
+        System.out.print("input element is :"+element);
+    }
+}
