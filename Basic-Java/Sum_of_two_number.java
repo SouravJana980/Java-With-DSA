66f7d23ab22eb92434a27c4aaa9d23ea6ce2bd53
SouravJana980 <sj493393@gmail.com>
2025-11-12 22:08:44 +0530

    Add Sum_of_two_number.java to calculate sum

---
+++ Basic-Java/Sum_of_two_number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Sum_of_two_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter input element1:");
+        int element1 = sc.nextInt();
+        System.out.print("enter input element2:");
+        int element2 = sc.nextInt();
+        int sum = element1+element2;
+        System.out.print(element1+" + "+element2+" is : "+sum);
+        
+    }
+}
