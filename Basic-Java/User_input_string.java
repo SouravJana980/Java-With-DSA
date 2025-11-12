ca6cd1f1bfad368bdd1ba4502fe9c17cecf23554
SouravJana980 <sj493393@gmail.com>
2025-11-12 22:08:19 +0530

    Add User_input_string class for user input

---
+++ Basic-Java/User_input_string.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+
+public class User_input_string {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter name:");
+        String str = sc.nextLine();
+        System.out.print("input string str:"+str);
+    }
+
+}
