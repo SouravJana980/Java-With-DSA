a971bd6f881ecc80fba75d5457702ff03c8d2fc6
SouravJana980 <sj493393@gmail.com>
2025-11-13 23:11:56 +0530

    Add Pirnt_hello_3times.java to print 'hello' 3 times

---
+++ Loop-In-Java/Pirnt_hello_3times.java
@@ -0,0 +1,9 @@
+public class Pirnt_hello_3times {
+
+    //here using for loop
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            System.out.println("hello");
+        }
+    }
+}
