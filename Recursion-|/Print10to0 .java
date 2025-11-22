ce47e7543590d622d8ee0ee588b4f02e0a7edb0d
SouravJana980 <sj493393@gmail.com>
2025-11-22 22:24:22 +0530

    Create Print10to0 .java

---
+++ Recursion-|/Print10to0 .java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Print10to0 {
+
+    public static void printDec(int n){
+        if(n == 1){
+            System.out.println(1);
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number n is:");
+        int n = sc.nextInt(); 
+        printDec(n);
+    }
+}
